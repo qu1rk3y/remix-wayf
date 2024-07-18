b43e47e589bf630bffa98f8b7c121183dd5f4d8f
Ollie Quirke <ollie@quir.ke>
2024-07-18 13:13:49 +0100

    Change supabase uri

--- app/api/supabase.ts
+++ app/api/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
-const SUPABASE_URL = "https://zwlxxknypnpeildsqprf.supabase.co";
+const SUPABASE_URL = "https://uhfezobwivggqvwofaph.supabase.co";
 
 if (!SUPABASE_KEY) {
-    throw new Error("Missing SUPABASE_KEY");
+  throw new Error("Missing SUPABASE_KEY");
 }
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
